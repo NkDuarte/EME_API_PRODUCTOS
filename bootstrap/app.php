1ba8496ba7008cf02f1b9f7c61ff9c5ed05d7cb5
NkDuarte <nkduarte22@gmail.com>
2025-01-13 16:21:45 -0500

    FIX 🫢 |-> Correccion de middleware csrf token

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            'https://emeapiproductos-production.up.railway.app/api/products/*',
-            'https://emeapiproductos-production.up.railway.app/api/products'
+            '*'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
