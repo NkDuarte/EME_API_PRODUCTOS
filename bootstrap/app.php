f397440794d91d7d38f9d32d6a11d1ce8415c3ee
NkDuarte <nkduarte22@gmail.com>
2025-01-13 16:00:19 -0500

    FIX 🫢 |-> Correccion de variables en produccion para el token csrf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,15 +6,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $url_prod = "https://emeapiproductos-production.up.railway.app/";
+        $url_local = "http://localhost:8000";
+
         $middleware->validateCsrfTokens(except: [
-            'http://localhost:8000/api/products/*',
-            'http://localhost:8000/api/products'
+            $url_prod + '/api/products/*',
+            $url_prod + '/api/products'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
